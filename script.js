dad8e1c1f218b8775305da7c07031ea7f3fe40a9
MrSethi <iampssethi@gmail.com>
2019-01-15 16:46:40 +0530

    ccc

---
+++ script.js
@@ -0,0 +1,3 @@
+// console.log(document.getElementById('about').clientHeight);
+
+// document.getElementById("events").style.top = document.getElementById('slider').clientHeight+"px" +  document.getElementById('about').clientHeight+"px" + 65+"px";
